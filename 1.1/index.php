d68bbb4a87bf7b3bd27eae117224003301bfb3f9
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-08-26 18:31:17 +0200

    Update index.php

--- 1.1/index.php
+++ 1.1/index.php
@@ -1,53 +1,65 @@
+<?php
+header('Content-type: text/plain');
+//File count name.
+$filePath = "visitCount.txt";
 
-  <?php
-     header('Content-type: text/plain');
-     //File count name.
-     $filePath = "visitCount.txt";
+//Check existence of file, else try to create the file starting at count 0.
+if (file_exists($filePath)) {
+    readAndWriteFile($filePath);
+} else {
+    createFile($filePath);
+}
 
-     //Check existance of file, else try to create the file starting at count 0.
-     if(file_exists($filePath)){
-       readAndWriteFile($filePath);
-     }else{
-      createFile($filePath);
-     }
+/**
+ * Reads the file and increments the view count and then writes to the file.
+ * @param string $path
+ */
+function readAndWriteFile(string $path)
+{
+    //Open the specified file and enable
+    $file = fopen($path, "r+");
 
-     function readAndWriteFile($path){
-        //Open the specified file and enable
-        $file = fopen($path, "a+");
+    //Acquire exclusive lock (writer).
+    if (flock($file, LOCK_EX)) {
 
-        //Aquire exclusive lock (writer).
-        if(flock($file, LOCK_EX)){
+        //Read the content of the file.
+        $visitCount = fread($file, filesize($path));
 
-          //Read the content of the file.
-          $visitCount = fread($file, filesize($path));
+        //Increment count.
+        $visitCount += 1;
 
-          //Increment count.
-          $visitCount = $visitCount + 1;
+        //Truncate
+        ftruncate($file, 0);
 
-          //Display the visit count after incrementing it.
-          echo $visitCount;
+        //Write pointer to beginning of selected file.
+        rewind($file);
 
-          //Truncate
-          ftruncate($file, 0);
-          //Write pointer to beginning of selected file
-          rewind($file);
-          //Write updated count to server file.
-          fwrite($file, $visitCount);
-          // flush output before releasing the lock
-          fflush($file);
+        //Write updated count to server file.
+        fwrite($file, $visitCount);
 
-          //Release a lock(shared or exclusive).
-          flock($file, LOCK_UN);
-        }else{
-          echo "Could not get the lock!";
-        }
-        fclose($file);
-     }
+        // flush output before releasing the lock
+        fflush($file);
 
-     function createFile($path){
-         $createFile = fopen("visitCount.txt", "w");
-         file_put_contents($path, "0");
-         //Refresh site after file is created.
-         header('Refresh:0');
-     }
-  ?>
+        //Display the visit count after incrementing it.
+        echo $visitCount;
+
+        //Release the exclusive lock.
+        flock($file, LOCK_UN);
+    } else {
+        echo "Could not get the lock!";
+    }
+
+    fclose($file);
+}
+
+/**
+ * Creates a text file and sets the view count to 0.
+ * @param string $path
+ */
+function createFile(string $path)
+{
+    file_put_contents($path, 0);
+    //Refresh site after file is created.
+    header('Refresh:0');
+}
+
