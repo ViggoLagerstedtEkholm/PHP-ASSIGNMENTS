eb39ba0b9ed12d8052f4ab8a362b857b2aa5fb98
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:38:54 +0100

    Update display.php

--- 2.1/display.php
+++ 2.1/display.php
@@ -1,6 +1,11 @@
 <?php
+/**
+ * We set the header as text/plain and iterate over the GET array and extract the key -> value pair.
+ * @author Viggo Lagerstedt Ekholm
+ */
+
 header('Content-type: text/plain');
 
-echo "name: " . $_GET["name"] . "\n";
-echo "email: " .$_GET["email"] . "\n";
-?>
+foreach($_GET as $key => $value){
+    echo $key . ": " . $value . "\n";
+}
