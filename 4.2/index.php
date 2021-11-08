ee81e2f8609febebd03fe96b11a25ab35dc0424a
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:42:32 +0100

    Update index.php

--- 4.2/index.php
+++ 4.2/index.php
@@ -1,15 +1,25 @@
 <?php
-$session_id = generateRandom();
+/**
+ * This script creates a random session ID and creates a cookie with this session ID. After 3 hours the cookie expires.
+ * @author Viggo Lagerstedt Ekholm
+ */
 
-function generateRandom() {
-    return random_int(100000, 999999);
+try {
+    $session_id = generateRandom();
+    $html = file_get_contents("form.html");
+    $cookieName = "session_store_cookie";
+    setcookie($cookieName, $session_id, time() + 10800, "/"); //Create cookie with the session ID stored for 3 hours.
+    $html = str_replace('---session-id---', $session_id, $html);
+    echo $html;
+} catch (Exception $e) {
+    echo "Failed to generate ID!";
 }
 
-$html = file_get_contents("form.html");
-$cookieName = "session_store_cookie";
-setcookie($cookieName, $session_id, time() + 10800, "/");
-$html = str_replace('---session-id---', $session_id, $html);
-echo $html;
-
-
-?>
+/**
+ * Generate random session ID
+ * @throws Exception
+ */
+function generateRandom(): int
+{
+    return random_int(100000, 999999);
+}
