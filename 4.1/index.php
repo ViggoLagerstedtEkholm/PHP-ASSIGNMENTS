5b8993591dce9b31900852f3f7c30759b82dac7c
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:41:52 +0100

    Update index.php

--- 4.1/index.php
+++ 4.1/index.php
@@ -1,14 +1,24 @@
 <?php
-$rand = generateRandom();
-$html = file_get_contents("form.html");
-$html = str_replace('---session-id---', $rand, $html);
-echo $html;
 
-$rand = generateRandom();
+/**
+ * This script will replace the session id placeholder with randomly generated id.
+ * @author Viggo Lagerstedt Ekholm
+ */
 
-function generateRandom() {
-    return random_int(100000, 999999);
+try {
+    $rand = generateRandom();
+    $html = file_get_contents("form.html");
+    $html = str_replace('---session-id---', $rand, $html);
+    echo $html;
+} catch (Exception $e) {
+    echo "Failed to generate ID!";
 }
 
-
-?>
+/**
+ * Generate random session ID
+ * @throws Exception
+ */
+function generateRandom(): int
+{
+    return random_int(100000, 999999);
+}
