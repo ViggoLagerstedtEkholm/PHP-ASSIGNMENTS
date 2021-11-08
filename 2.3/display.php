924a88774919e02135a4e8cb8daefb39596573e9
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:40:07 +0100

    Update display.php

--- 2.3/display.php
+++ 2.3/display.php
@@ -1,56 +1,62 @@
 <?php
-if(isset($_POST["publish"])){
-  //Array of acceptable MIME-types.
-  $accepted_types = array("text/plain", "image/jpeg", "image/png");
+/**
+ *  This script takes an uploaded file and checks if no errors occurred and that the file does not exceed the size limit.
+ *  If we have jpg, png, text - display those (with the right headers).
+ *  Any other formats we just display name, type, size.
+ *  @author Viggo Lagerstedt Ekholm
+ */
 
-  //Gather file attributes.
-  $file = $_FILES['file'];
-  $fileType = $_FILES['file']['type'];
-  $fileName = $_FILES['file']['name'];
-  $fileSize = $_FILES['file']['size'];
-  $fileErr = $_FILES['file']['error'];
-  $fileTmpName = $_FILES['file']['tmp_name'];
+if (isset($_POST["publish"])) {
+    //Array of acceptable MIME-types.
+    $accepted_types = array("text/plain", "image/jpeg", "image/png");
 
-  $fileExt = explode('/', $fileName);
-  $fileActualExt = strtolower(end($fileExt));
+    //Gather file attributes.
+    $file = $_FILES['file'];
+    $fileType = $_FILES['file']['type'];
+    $fileName = $_FILES['file']['name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileErr = $_FILES['file']['error'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
 
-  //Check if file upload had any errors.
-  if($fileErr === 0){
-    //Enable max file size.
-    if($fileSize < 1000000){
-      //Create file path information.
-      $fileNameNew = uniqid('', true).".".$fileActualExt;
-      $fileDst = 'uploads/' . $fileNameNew;
+    $fileExt = explode('/', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
 
-      //Upload images to server.
-      move_uploaded_file($fileTmpName, $fileDst);
+    //Check if file upload had any errors.
+    if ($fileErr === 0) {
+        //Enable max file size.
+        if ($fileSize < 10000000) {
+            //Create file path information.
+            $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+            $fileDst = 'uploads/' . $fileNameNew;
 
-      //Show the appropriate file.
-      switch($fileType){
-        case "text/plain":
-        header('Content-type: text/plain');
-        $myfile = fopen($fileDst, "r") or die("Unable to open file!");
-        echo fread($myfile,filesize($fileDst));
-        fclose($myfile);
-        break;
-        case "image/jpeg":
-        header('Content-type: image/jpeg');
-        readfile($fileDst);
-        break;
-        case "image/png":
-        header('Content-type: image/png');
-        readfile($fileDst);
-        break;
-        default:
-        echo "name: " . $fileName . "<br>";
-        echo "type: " . $fileType . "<br>";
-        echo "size: " . $fileSize . "<br>";
-      }
-    }else{
-      echo "Whooosh, that's a big file! Choose another one that is smaller!";
+            //Upload images to server.
+            move_uploaded_file($fileTmpName, $fileDst);
+
+            //Show the appropriate file.
+            switch ($fileType) {
+                case "text/plain":
+                    header('Content-type: text/plain');
+                    $text = fopen($fileDst, "r") or die("Unable to open file!");
+                    echo fread($text, filesize($fileDst));
+                    fclose($text);
+                    break;
+                case "image/jpeg":
+                    header('Content-type: image/jpeg');
+                    readfile($fileDst);
+                    break;
+                case "image/png":
+                    header('Content-type: image/png');
+                    readfile($fileDst);
+                    break;
+                default:
+                    echo "name: " . $fileName . ", ";
+                    echo "type: " . $fileType . ", ";
+                    echo "size: " . $fileSize . ".";
+            }
+        } else {
+            echo "Whoosh, that's a big file! Choose another one that is smaller!";
+        }
+    } else {
+        echo "Error uploading file!";
     }
-  }else{
-    echo "Error uploading file!";
-  }
 }
-?>
