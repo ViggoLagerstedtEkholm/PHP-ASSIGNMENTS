9101d9b6d19f01adb14c9b6f6b2913236d6954f6
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:43:16 +0100

    Update index.php

--- 4.3/index.php
+++ 4.3/index.php
@@ -1,10 +1,15 @@
 <?php
-session_start();
+/**
+ * This script uses the built in session tools.
+ * I save the generated session id in a cookie.
+ * @author Viggo Lagerstedt Ekholm
+ */
+session_start(); //Start the session.
+session_regenerate_id(); //Regenerate the session ID.
+
 $html = file_get_contents("form.html");
 $cookieName = "session_store_cookie";
-$session_id = session_id();
-setcookie($cookieName, $session_id, time() + 10800, "/");
+$session_id = session_id(); //Get the generated session ID.
+setcookie($cookieName, $session_id, time() + 10800, "/"); //Create a cookie with the generated session ID that expires in 3 hours.
 $html = str_replace('---session-id---', $session_id, $html);
 echo $html;
-session_regenerate_id(true);
-?>
