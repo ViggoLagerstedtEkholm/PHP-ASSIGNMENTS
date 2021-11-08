05864f2c3f9ddc4e7d38b8acdf7afa88ad118b88
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:39:34 +0100

    Update response.php

--- 2.2/response.php
+++ 2.2/response.php
@@ -1,24 +1,22 @@
 <?php
+/**
+ *  This script iterates over the POST and GET arrays and prints the HTTP header type followed by the key to value pair.
+ *  @author Viggo Lagerstedt Ekholm
+ */
+
+//Set return header as text/plain.
 header('Content-type: text/plain');
 
-//Input names.
-$inputs = array("name", "password", "email", "checkbox", "select_items");
-
-//Loop through the input names and echo the method type followed by the input value.
-foreach($inputs as $input)
-{
-  if(isset($_POST[$input]))
-  {
+//Loop through the GET array and echo the HTTP header type followed by the key to value. (POST)
+foreach ($_POST as $key => $value) {
     echo "METHOD TYPE: POST \n";
-    echo $input . ": " . $_POST[$input] . "\n";
+    echo $key . ": " . $value . "\n";
     echo "\n";
-  }
-  else if(isset($_GET[$input]))
-  {
-    echo "METHOD TYPE: GET \n";
-    echo $input . ": " . $_GET[$input] . "\n";
-    echo "\n";
-  }
 }
 
-?>
+//Loop through the GET array and echo the HTTP header type followed by the key to value. (GET)
+foreach ($_GET as $key => $value) {
+    echo "METHOD TYPE: GET \n";
+    echo $key . ": " . $value . "\n";
+    echo "\n";
+}
