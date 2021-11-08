170f53a147845517155de1e5d77d0ecbbe79a23c
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:45:32 +0100

    Update IMAPReader.php

--- 5.3/IMAPReader.php
+++ 5.3/IMAPReader.php
@@ -1,67 +1,92 @@
 <?php
-//This class will handler all IMAP API calls and provide help to fetch the email from the mail server.
-class IMAPReader{
-  public $connection;
-  public $inbox;
-  public $msg_cnt;
-  public $server;
-  public $user;
-  public $pass;
-  public $port;
 
-  public function __construct($username, $password, $server, $port){
-    $this->user = $username;
-    $this->pass = $password;
-    $this->port = $port;
-    $this->server = $server;
+/**
+ * This class will handle all IMAP API calls and provide help to fetch the email from the mail server.
+ * @author Viggo Lagestedt Ekholm
+ */
+class IMAPReader
+{
+    public mixed $connection;
+    public array $inbox;
+    public int $msg_cnt;
+    public string $server;
+    public string $user;
+    public string $pass;
+    public int $port;
 
-    //Connect to the database and populate the inbox.
-    $this->connect();
-    $this->inbox();
-  }
+    public function __construct(string $username, string $password, string $server, int $port)
+    {
+        $this->user = $username;
+        $this->pass = $password;
+        $this->port = $port;
+        $this->server = $server;
 
-  public function connect(){
-    //Compose the full server path name.
-    $full_server_name = '{' .$this->server. ':'. $this->port .'/imap/ssl/novalidate-cert}';
-    //Try to connect, if it fails it will print the error.
-    $this->connection = imap_open('{' .$this->server. ':'. $this->port .'/imap/ssl/novalidate-cert}', $this->user, $this->pass) or die('Cannot connect to Mail: ' . imap_last_error());
-  }
-
-  public function close(){
-    $this->inbox = array();
-  }
-
-  public function inbox(){
-    $this->msg_cnt = imap_num_msg($this->connection);
-
-    //Create 2D array, populate it with all emails in the server.
-    $in = array();
-    for($i = 1; $i <= $this->msg_cnt; $i++){
-      $in[] = array(
-        'index' => $i,
-        'header' => imap_headerinfo($this->connection, $i),
-        'body_raw' => imap_body($this->connection, $i),
-        'body_text' => imap_fetchbody($this->connection, $i, 1),
-        'body_html' => imap_fetchbody($this->connection, $i, 2),
-        'structure' => imap_fetchstructure($this->connection, $i)
-      );
+        //Connect to the database and populate the inbox.
+        $this->connect();
+        $this->inbox();
     }
-    //Assign the inbox variable.
-    $this->inbox = $in;
-  }
 
-  //This method will help to get the inbox index value, it will be used in index.php to get all emails!
-  public function GetMessageAtIndex($msg_index = NULL){
-    if(count($this->inbox) <= 0){
-      return array();
-    }else if(!is_null($msg_index) && isset($this->inbox[$msg_index])){
-      return $this->inbox[$msg_index];
+    /**
+     * This method connects to the database using the server-name/username/password/port.
+     */
+    public function connect()
+    {
+        $full_server_name = '{' . $this->server . ':' . $this->port . '/imap/ssl/novalidate-cert}';
+        $this->connection = imap_open($full_server_name, $this->user, $this->pass) or die('Cannot connect to Mail: ' . imap_last_error());
     }
-    return $this->inbox[0];
-  }
 
-  //Returns the email/message count.
-  public function GetMessageCount(){
-    return $this->msg_cnt;
-  }
+    /**
+     * Empty the inbox.
+     */
+    public function close()
+    {
+        $this->inbox = array();
+    }
+
+    /**
+     * This method will iterate over all the messages the user has in the mail box and add them to the 2D array.
+     */
+    public function inbox()
+    {
+        $this->msg_cnt = imap_num_msg($this->connection);
+
+        //Create 2D array, populate it with all emails in the server. Using the IMAP api we can fetch different information.
+        $in = array();
+        for ($i = 1; $i <= $this->msg_cnt; $i++) {
+            $in[] = array(
+                'index' => $i,
+                'header' => imap_headerinfo($this->connection, $i),
+                'body_raw' => imap_body($this->connection, $i),
+                'body_text' => imap_fetchbody($this->connection, $i, 1),
+                'body_html' => imap_fetchbody($this->connection, $i, 2),
+                'structure' => imap_fetchstructure($this->connection, $i)
+            );
+        }
+
+        $this->inbox = $in;
+    }
+
+    /**
+     * This method will help to get the inbox index value.
+     * @param int|null $msg_index
+     * @return array
+     */
+    public function GetMessageAtIndex(int $msg_index = NULL): array
+    {
+        if (count($this->inbox) <= 0) {
+            return array();
+        } else if (!is_null($msg_index) && isset($this->inbox[$msg_index])) {
+            return $this->inbox[$msg_index];
+        }
+        return $this->inbox[0];
+    }
+
+    /**
+     * Get the total amount of messages in the inbox.
+     * @return int
+     */
+    public function GetMessageCount(): int
+    {
+        return $this->msg_cnt;
+    }
 }
