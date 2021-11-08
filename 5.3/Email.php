7d6b78c3896204557d7144d97de14447af73bf30
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2021-11-09 00:45:51 +0100

    Update Email.php

--- 5.3/Email.php
+++ 5.3/Email.php
@@ -1,31 +1,42 @@
 <?php
-class Email{
-  private $from_address;
-  private $date;
-  private $body_text;
-  private $subject;
 
-  public function __construct($from_address, $date, $body_text, $subject){
-    $this->from_address = $from_address;
-    $this->date = $date;
-    $this->body_text = $body_text;
-    $this->subject = $subject;
-  }
+/**
+ * Email model containing fields for an email.
+ * @author Viggo Lagestedt Ekholm
+ */
+class Email
+{
+    private string $from_address;
+    private string $date;
+    private string $body_text;
+    private string $subject;
 
-  public function GetFromAddress(){
-    return $this->from_address;
-  }
+    public function __construct(string $from_address, string $date, string $body_text, string $subject)
+    {
+        $this->from_address = $from_address;
+        $this->date = $date;
+        $this->body_text = $body_text;
+        $this->subject = $subject;
+    }
 
-  public function GetDate(){
-    return $this->date;
-  }
+    public function GetFromAddress(): string
+    {
+        return $this->from_address;
+    }
 
-  public function GetBodyText(){
-    return $this->body_text;
-  }
+    public function GetDate(): string
+    {
+        return $this->date;
+    }
 
-  public function GetSubject(){
-    return $this->subject;
-  }
+    public function GetBodyText(): string
+    {
+        return $this->body_text;
+    }
+
+    public function GetSubject(): string
+    {
+        return $this->subject;
+    }
 }
-?>
+
